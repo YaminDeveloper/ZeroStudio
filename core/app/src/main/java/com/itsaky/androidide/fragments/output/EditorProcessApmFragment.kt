003ab9434d770f4be73f71a9d92c271285237771
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-04-25 09:23:07 +0800

    Opt in to experimental Material3 TopAppBar API in APM fragment

--- core/app/src/main/java/com/itsaky/androidide/fragments/output/EditorProcessApmFragment.kt
+++ core/app/src/main/java/com/itsaky/androidide/fragments/output/EditorProcessApmFragment.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -129,6 +130,7 @@ class EditorProcessApmFragment : Fragment() {
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 private fun EditorApmMonitorScreen(
     snapshot: EditorProcessApmSnapshot?,
     onCleanProcesses: () -> Unit,
